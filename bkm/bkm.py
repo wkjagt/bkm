d78a02041eb481d12621496f2098e5f9ff424767
Willem van der Jagt <wkjagt@gmail.com>
2014-08-11 23:15:39 -0400

    making things slightly more modular

--- bkm/bkm.py
+++ bkm/bkm.py
@@ -3,138 +3,165 @@ import click, webbrowser, ConfigParser, os
 
 CONFIG_FILE = os.path.join(os.path.expanduser("~"), '.bkm')
 
+class Output(object):
+
+    def error(self, msg):
+        click.echo(click.style(msg, bg='red', fg='white'))
+        exit()
+
+    def success(self, msg):
+        click.echo(click.style(msg, fg='green'))
+
+    def info(self, msg):
+        click.echo(click.style(msg, fg='yellow'))
+
+    def as_columns(self, rows):
+        widths = [max(map(len, col)) for col in zip(*rows)]
+        for row in rows:
+            self.info("  ".join((val.ljust(width) for val, width in zip(row, widths))))
+
+
+class Config(ConfigParser.ConfigParser, Output):
+
+    def __init__(self, config_file_path, config_file, *args, **kwargs):
+        ConfigParser.ConfigParser.__init__(self, *args, **kwargs)
+        self.config_file = config_file
+        self.read(config_file_path)
+        if not self.has_section('bookmarks'):
+            self.add_section('bookmarks')
+
+    def assert_bookmark_exists(self, bookmark):
+        if not self.has_option('bookmarks', bookmark):
+            self.error('Bookmark "{0}" doesn\'t exist'.format(bookmark))
+
+    def get_bookmark_url(self, bookmark):
+        return self.get('bookmarks', bookmark)
+
+    def check_bookmark_exists(self, bookmark):
+        if not self.has_option('bookmarks', bookmark):
+            self.error('Bookmark "{0}" doesn\'t exist'.format(bookmark))
+
+    def check_bookmark_doesnt_exist(self, bookmark):
+        if self.has_option('bookmarks', bookmark):
+            self.error('Bookmark "{0}" exist'.format(bookmark))
+
+    def get_bookmarks(self):
+        return self.options('bookmarks')
+
+    def save_bookmark(self, bookmark, url):
+        self.set('bookmarks', bookmark, url)
+        self.write_config_file()
+
+    def delete_bookmark(self, bookmark):
+        self.remove_option('bookmarks', bookmark)
+        self.write_config_file()
+
+    def write_config_file(self):
+        self.write(self.config_file)
+
+
+class BKM(Output):
+
+    def __init__(self, config):
+        self.config = config
+
+    def open(self, bookmarks):
+        if len(bookmarks) > 0:
+            for bookmark in bookmarks:
+                self.config.check_bookmark_exists(bookmark)
+            for bookmark in bookmarks:
+                self.open_bookmark(bookmark)
+        else:
+            self.select_from_list()
+
+    def add(self, bookmark, url):
+
+        self.config.check_bookmark_doesnt_exist(bookmark)
+        self.config.save_bookmark(bookmark, url)
+        self.success('"{0}" added for "{1}"'.format(bookmark, url))
+
+    def select_from_list(self):
+        self.list_bookmarks()
+        idx = click.prompt('Enter the number of the bookmark to open', type=int)
+        try:
+            bookmark = self.config.options('bookmarks')[idx-1]
+            self.open_bookmark(bookmark)
+        except Exception, e:
+            print e
+            self.error('{0} is not a valid option'.format(idx))
+
+    def list_bookmarks(self):
+        rows = []
+        for i, bookmark in enumerate(self.config.get_bookmarks()):
+            rows.append([str(i+1), bookmark, self.config.get('bookmarks', bookmark)])
+
+        self.as_columns(rows)
+
+    def open_bookmark(self, bookmark):
+        try:
+            webbrowser.open(self.config.get_bookmark_url(bookmark), new=2)
+        except ConfigParser.NoOptionError:
+            self.error('No bookmark named "{0}"'.format(bookmark))
+
+    def show(self, bookmark):
+        self.config.check_bookmark_exists(bookmark)
+        self.info('Bookmark "{0}" : "{1}"'.format(bookmark, self.config.get_bookmark_url(bookmark)))
+
+    def list(self):
+        self.list_bookmarks()
+
+    def change(self, bookmark, url):
+        self.config.check_bookmark_exists(bookmark)
+        self.config.save_bookmark(bookmark, url)
+        self.success('"{0}" changed to "{1}"'.format(bookmark, url))
+
+    def remove(self, bookmark):
+        self.config.check_bookmark_exists(bookmark)
+        self.config.delete_bookmark(bookmark)
+        self.success('Bookmark "{0}" deleted'.format(bookmark))
+
 @click.group()
 @click.pass_context
 @click.option('--config_file', type=click.File('w+'), default=CONFIG_FILE)
 def cli(ctx, config_file):
-
-    ctx.obj = {
-        'config' : ConfigParser.ConfigParser(),
-        'config_file' : config_file
-    }
-    ctx.obj['config'].read(CONFIG_FILE)
-
-    __bookmarks_section_exists(ctx)
+    ctx.obj = {'bkm' : BKM(Config(CONFIG_FILE, config_file))}
 
 @cli.command()
 @click.pass_context
 @click.argument('bookmarks', nargs=-1)
-def open(ctx, bookmarks):
-
-    if len(bookmarks) > 0:
-        for bookmark in bookmarks:
-            __check_bookmark_exists(ctx, bookmark)
-        for bookmark in bookmarks:
-            __open_bookmark(ctx, bookmark)
-    else:
-        __select_from_list(ctx)
+def open(ctx, *args, **kwargs):
+    getattr(ctx.obj['bkm'], ctx.command.name)(*args, **kwargs)
 
 @cli.command()
 @click.pass_context
 @click.argument('bookmark', nargs=1)
 @click.argument('url', nargs=1)
-def add(ctx, bookmark, url):
-    __check_bookmark_doesnt_exist(ctx, bookmark)
-    __save_bookmark(ctx, bookmark, url)
-    __success('"{0}" added for "{1}"'.format(bookmark, url))
+def add(ctx, *args, **kwargs):
+    getattr(ctx.obj['bkm'], ctx.command.name)(*args, **kwargs)
 
 @cli.command()
 @click.pass_context
 @click.argument('bookmark', nargs=1)
-def show(ctx, bookmark):
-    __check_bookmark_exists(ctx, bookmark)
-    __show_bookmark(ctx, bookmark)
+def show(ctx, *args, **kwargs):
+    getattr(ctx.obj['bkm'], ctx.command.name)(*args, **kwargs)
 
 @cli.command()
 @click.pass_context
-def list(ctx):
-    __list_bookmarks(ctx)
-
+def list(ctx, *args, **kwargs):
+    getattr(ctx.obj['bkm'], ctx.command.name)(*args, **kwargs)
 
 @cli.command()
 @click.pass_context
 @click.argument('bookmark', nargs=1)
 @click.argument('url', nargs=1)
-def change(ctx, bookmark, url):
-    __check_bookmark_exists(ctx, bookmark)
-    __save_bookmark(ctx, bookmark, url)
-    __success('"{0}" changed to "{1}"'.format(bookmark, url))
+def change(ctx, *args, **kwargs):
+    getattr(ctx.obj['bkm'], ctx.command.name)(*args, **kwargs)
 
 @cli.command()
 @click.pass_context
 @click.argument('bookmark', nargs=1)
-def remove(ctx, bookmark):
-    __check_bookmark_exists(ctx, bookmark)
-    __delete_bookmark(ctx, bookmark)
-    __success('Bookmark "{0}" deleted')
-
-def __save_bookmark(ctx, bookmark, url):
-    ctx.obj['config'].set('bookmarks', bookmark, url)
-    __write_config_file(ctx)
-
-def __delete_bookmark(ctx, bookmark):
-    ctx.obj['config'].remove_option('bookmarks', bookmark)
-    __write_config_file(ctx)
-
-def __write_config_file(ctx):
-    ctx.obj['config'].write(ctx.obj['config_file'])
-
-def __check_bookmark_exists(ctx, bookmark):
-    if not ctx.obj['config'].has_option('bookmarks', bookmark):
-        __error('Bookmark "{0}" doesn\'t exist'.format(bookmark))
-
-def __check_bookmark_doesnt_exist(ctx, bookmark):
-    if ctx.obj['config'].has_option('bookmarks', bookmark):
-        __error('Bookmark "{0}" exist'.format(bookmark))
-
-def __show_bookmark(ctx, bookmark):
-    url = __get_bookmark_url(ctx, bookmark)
-    __info('Bookmark "{0}" : "{1}"'.format(bookmark, url))
-
-def __list_bookmarks(ctx):
-    rows = []
-    for i, bookmark in enumerate(ctx.obj['config'].options('bookmarks')):
-        rows.append([str(i+1), bookmark, ctx.obj['config'].get('bookmarks', bookmark)])
-
-    widths = [max(map(len, col)) for col in zip(*rows)]
-    for row in rows:
-        __info("  ".join((val.ljust(width) for val, width in zip(row, widths))))
-
-def __get_bookmark_url(ctx, bookmark):
-    return ctx.obj['config'].get('bookmarks', bookmark)
-
-def __open_bookmark(ctx, bookmark):
-    try:
-        url = __get_bookmark_url(ctx, bookmark)
-        webbrowser.open(url, new=2)
-    except ConfigParser.NoOptionError:
-        __error('No bookmark named "{0}"'.format(bookmark))
-
-def __bookmarks_section_exists(ctx):
-    if not ctx.obj['config'].has_section('bookmarks'):
-        __add_section(ctx, 'bookmarks')
-
-def __select_from_list(ctx):
-    __list_bookmarks(ctx)
-    idx = click.prompt('Enter the number of the bookmark to open', type=int)
-    try:
-        bookmark = ctx.obj['config'].options('bookmarks')[idx-1]
-        __open_bookmark(ctx, bookmark)
-    except Exception, e:
-        __error('{0} is not a valid option'.format(idx))
-
-def __add_section(ctx, section):
-    ctx.obj['config'].add_section(section)
-
-def __error(msg):
-    click.echo(click.style(msg, bg='red', fg='white'))
-    exit()
-
-def __success(msg):
-    click.echo(click.style(msg, fg='green'))
-
-def __info(msg):
-    click.echo(click.style(msg, fg='yellow'))
+def remove(ctx, *args, **kwargs):
+    getattr(ctx.obj['bkm'], ctx.command.name)(*args, **kwargs)
 
 if __name__ == '__main__':
     cli()
